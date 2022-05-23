cd0603f8417d787c7989f133925439dde3c9a970
Andpedroso <ankpedroso@gmail.com>
2022-05-23 17:07:41 -0300

    API do projeto Chapter com o get

---
+++ SENAI-UC14-E1/SENAI-UC14-E1/Repositories/LivroRepository.cs
@@ -0,0 +1,20 @@
+﻿using SENAI_UC14_E1.Contexts;
+using SENAI_UC14_E1.Models;
+
+namespace SENAI_UC14_E1.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly ChapterContext _context;
+
+        public LivroRepository(ChapterContext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
